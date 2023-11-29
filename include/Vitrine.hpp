538e83f5eaf782c443209a7b8fc076bd11b48694
Caio Caracas <caiocaracas1@gmail.com>
2023-11-29 00:52:10 -0300

    vitrine

---
+++ include/Vitrine.hpp
@@ -0,0 +1,17 @@
+#ifndef VITRINE_HPP
+#define VITRINE_HPP
+
+#include <list>
+#include "Produto.hpp"
+
+class Vitrine {
+public:
+  void adicionarProduto(const Produto& produto);
+  void removerProduto(const Produto& produto);
+  const std::list<Produto>& obterProdutos() const;
+
+private:
+  std::list<Produto> produtosNaVitrine;
+};
+
+#endif
